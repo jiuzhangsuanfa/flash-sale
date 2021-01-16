d1fac5f4f2e21bc84340e50437dd85d45855c93e
Devin Don <i.inf@outlook.com>
2021-01-17 01:03:38 +0800

    perf(service/html): optimize try resource

--- src/main/java/com/jiuzhang/flashsale/service/ActivityHtmlPageService.java
+++ src/main/java/com/jiuzhang/flashsale/service/ActivityHtmlPageService.java
@@ -35,37 +35,25 @@ public class ActivityHtmlPageService {
      */
     public void createActivityHtml(long activityId) {
 
-        PrintWriter writer = null;
-        try {
+        try (PrintWriter writer = new PrintWriter(
+                new File("src/main/resources/templates/" + "seckill_item_" + activityId + ".html"))) {
             Activity activity = activityService.getById(activityId);
             Commodity commodity = commodityService.getById(activity.getCommodityId());
             // 获取页面数据
             Map<String, Object> resultMap = new HashMap<>();
             resultMap.put("activity", activity);
             resultMap.put("commodity", commodity);
-            // resultMap.put("seckillPrice", seckillActivity.getSeckillPrice());
-            // resultMap.put("oldPrice", seckillActivity.getOldPrice());
-            // resultMap.put("commodityId", seckillActivity.getCommodityId());
-            // resultMap.put("commodityName", seckillCommodity.getCommodityName());
-            // resultMap.put("commodityDesc", seckillCommodity.getCommodityDesc());
-
             // 创建thymeleaf上下文对象
             Context context = new Context();
             // 把数据放入上下文对象
             context.setVariables(resultMap);
-
-            // 创建输出流
-            File file = new File("src/main/resources/templates/" + "seckill_item_" + activityId + ".html");
-            writer = new PrintWriter(file);
             // 执行页面静态化方法
             templateEngine.process("seckill_item", context, writer);
         } catch (Exception e) {
             log.error(e.toString());
             log.error("页面静态化出错：" + activityId);
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
         }
+
     }
+
 }
