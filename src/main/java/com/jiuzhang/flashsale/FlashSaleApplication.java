7327647663834f08ea3a2e098e912f5903402187
Devin Don <i.inf@outlook.com>
2021-01-17 01:21:20 +0800

    refactor(main): rename entry class

--- src/main/java/com/jiuzhang/flashsale/FlashSaleApplication.java
+++ src/main/java/com/jiuzhang/flashsale/FlashSaleApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.jiuzhang.flashsale.mapper")
 @SpringBootApplication
-public class SeckillApplication {
+public class FlashSaleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SeckillApplication.class, args);
+        SpringApplication.run(FlashSaleApplication.class, args);
     }
 
 }
