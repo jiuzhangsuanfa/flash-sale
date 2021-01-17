d1f2fec8a6cfb36d16e6ac257e5dd2536bf6c2e7
Devin Don <i.inf@outlook.com>
2021-01-17 21:51:19 +0800

    refactor(service/rocketmq): refactor rocketmq service

--- src/main/java/com/jiuzhang/flashsale/service/RocketMQService.java
+++ src/main/java/com/jiuzhang/flashsale/service/RocketMQService.java
@@ -1,15 +1,32 @@
 package com.jiuzhang.flashsale.service;
 
-import org.apache.rocketmq.client.exception.MQBrokerException;
-import org.apache.rocketmq.client.exception.MQClientException;
-import org.apache.rocketmq.remoting.exception.RemotingException;
+import com.jiuzhang.flashsale.exception.MQException;
 
+/**
+ * Rocket MQ 服务
+ *
+ * @author jiuzhang
+ * @since 2021-01-17
+ */
 public interface RocketMQService {
 
-    public void sendMessage(String topic, String body)
-            throws MQClientException, RemotingException, MQBrokerException, InterruptedException;
+    /**
+     * 发送消息
+     *
+     * @param topic 消息通道（主题）
+     * @param body  消息内容
+     * @throws MQException 发送消息失败的异常
+     */
+    public void sendMessage(String topic, String body) throws MQException;
 
-    public void sendDelayMessage(String topic, String body, int delayTimeLevel)
-            throws MQClientException, RemotingException, MQBrokerException, InterruptedException;
+    /**
+     * 发送延迟消息
+     *
+     * @param topic          消息通道（主题）
+     * @param body           消息内容
+     * @param delayTimeLevel 延迟消息等级
+     * @throws MQException 发送消息失败的异常
+     */
+    public void sendDelayMessage(String topic, String body, int delayTimeLevel) throws MQException;
 
 }
--- src/main/java/com/jiuzhang/flashsale/service/impl/RocketMQServiceImpl.java
+++ src/main/java/com/jiuzhang/flashsale/service/impl/RocketMQServiceImpl.java
@@ -2,46 +2,36 @@ package com.jiuzhang.flashsale.service.impl;
 
 import javax.annotation.Resource;
 
+import com.jiuzhang.flashsale.exception.MQException;
 import com.jiuzhang.flashsale.service.RocketMQService;
 
-import org.apache.rocketmq.client.exception.MQBrokerException;
-import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.message.Message;
-import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RocketMQServiceImpl implements RocketMQService {
+
     @Resource
     RocketMQTemplate rocketMQTemplate;
 
-    /**
-     * 发送消息
-     *
-     * @param topic
-     * @param body
-     * @throws Exception
-     */
-    public void sendMessage(String topic, String body)
-            throws MQClientException, RemotingException, MQBrokerException, InterruptedException {
-        Message message = new Message(topic, body.getBytes());
-        rocketMQTemplate.getProducer().send(message, 100000);
+    public void sendMessage(String topic, String body) throws MQException {
+        try {
+            Message message = new Message(topic, body.getBytes());
+            rocketMQTemplate.getProducer().send(message, 100000);
+        } catch (Exception exception) {
+            throw new MQException(topic, exception.getMessage());
+        }
     }
 
-    /**
-     * 发送延时消息
-     *
-     * @param topic
-     * @param body
-     * @param delayTimeLevel
-     * @throws Exception
-     */
-    public void sendDelayMessage(String topic, String body, int delayTimeLevel)
-            throws MQClientException, RemotingException, MQBrokerException, InterruptedException {
-        Message message = new Message(topic, body.getBytes());
-        message.setDelayTimeLevel(delayTimeLevel);
-        rocketMQTemplate.getProducer().send(message);
+    public void sendDelayMessage(String topic, String body, int delayTimeLevel) throws MQException {
+        try {
+            Message message = new Message(topic, body.getBytes());
+            message.setDelayTimeLevel(delayTimeLevel);
+            rocketMQTemplate.getProducer().send(message);
+        } catch (Exception exception) {
+            throw new MQException(topic, exception.getMessage());
+        }
     }
 
 }
